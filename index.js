2dff53732988fd6dd84b99bd8769b6fa4b882ce0
Alvin J. Alexander <sandeepkumar.hexaware@gmail.com>
2017-12-04 11:11:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/public")));
+app.get("/", function (req, res) {
+    console.log("GET");
+    res.sendFile(__dirname+'/public/Welcome.vxml');
+})
+app.post("/", function (req, res) {
+    console.log("POST");
+    res.contentType('application/xml');
+    res.sendFile(__dirname+'/public/Welcome.vxml');
+})
+app.listen(process.env.PORT||3000);
